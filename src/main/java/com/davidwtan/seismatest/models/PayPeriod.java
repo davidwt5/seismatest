e2cb61ff804300808bf9a929ec10e01f149f9bd0
davidwt5 <davidwtan75@gmail.com>
2022-05-21 21:03:04 +1000

    added PayPeriod class

--- src/main/java/com/davidwtan/seismatest/models/PayPeriod.java
+++ src/main/java/com/davidwtan/seismatest/models/PayPeriod.java
@@ -1,2 +1,32 @@
-package com.davidwtan.seismatest.models;public class PayPeriod {
+package com.davidwtan.seismatest.models;
+
+public class PayPeriod {
+    private String fromDate;
+    private String toDate;
+
+    public PayPeriod(String fromDate, String toDate) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    @Override
+    public String toString() {
+        return fromDate + " - " + toDate;
+    }
 }
