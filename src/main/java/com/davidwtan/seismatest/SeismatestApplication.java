73b5e8faaeaf600aa98b20cb090695d35f567dfa
davidwt5 <davidwtan75@gmail.com>
2022-05-21 18:37:20 +1000

    initial commit

---
+++ src/main/java/com/davidwtan/seismatest/SeismatestApplication.java
@@ -0,0 +1,13 @@
+package com.davidwtan.seismatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeismatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeismatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidwtan/seismatest/SeismatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidwtan.seismatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeismatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
