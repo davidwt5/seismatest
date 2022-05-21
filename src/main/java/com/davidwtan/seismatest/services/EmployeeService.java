82cf3d5aef164541b9b7aa14ab094b6a51d58eaa
davidwt5 <davidwtan75@gmail.com>
2022-05-21 22:19:29 +1000

    implemented calcPayPeriod

--- src/main/java/com/davidwtan/seismatest/services/EmployeeService.java
+++ src/main/java/com/davidwtan/seismatest/services/EmployeeService.java
@@ -2,9 +2,15 @@ package com.davidwtan.seismatest.services;
 
 import com.davidwtan.seismatest.interfaces.PayslipGenerator;
 import com.davidwtan.seismatest.models.Employee;
+import com.davidwtan.seismatest.models.PayPeriod;
 import com.davidwtan.seismatest.models.Payslip;
+import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAdjuster;
 import java.util.List;
 
 @Service
@@ -14,8 +20,21 @@ public class EmployeeService implements PayslipGenerator {
         return null;
     }
 
-    // Don't forget to round
-    private int calcPayPeriod() {
+    // https://stackoverflow.com/questions/28177370/how-to-format-localdate-to-string
+    // https://stackoverflow.com/questions/13624442/getting-last-day-of-the-month-in-a-given-string-date
+    // Assumption: paymentMonth = 0 = January, 2 = February, ..., 11 = December
+    private PayPeriod calcPayPeriod(int paymentMonth) {
+        var thisYear = LocalDate.now().getYear();
+        var startOfMonth = LocalDate.of(thisYear, paymentMonth, 1);
+        var endOfMonth = startOfMonth.withDayOfMonth(
+                startOfMonth.getMonth().length(startOfMonth.isLeapYear()));
+        var formatter = DateTimeFormatter.ofPattern("dd LLLL");
+        return new PayPeriod(
+                startOfMonth.format(formatter),
+                endOfMonth.format(formatter));
+    }
+
+    private int grossIncome(int annualSalary) {
         return -1;
     }
 
