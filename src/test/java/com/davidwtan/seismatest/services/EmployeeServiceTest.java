a575288563d4d642ed164a4e4c9462a49a919bac
davidwt5 <davidwtan75@gmail.com>
2022-05-22 15:16:35 +1000

    added some more unit tests

--- src/test/java/com/davidwtan/seismatest/services/EmployeeServiceTest.java
+++ src/test/java/com/davidwtan/seismatest/services/EmployeeServiceTest.java
@@ -4,12 +4,161 @@ import com.davidwtan.seismatest.models.Employee;
 import com.davidwtan.seismatest.models.Payslip;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeServiceTest {
+    @Test
+    void baseTest() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 0, 0, 0)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0)
+        );
+    }
+
+    @Test
+    void september30DaysInMonth() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 0, 8, 0)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 September"),
+                Collections.singletonList("30 September"),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0)
+        );
+    }
+
+    @Test
+    void thisFebruaryLeapYearTest() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 0, 1, 0)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+
+        var daysInFeb = LocalDate.now().isLeapYear() ? 29 : 28;
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 February"),
+                Collections.singletonList(daysInFeb + " February"),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0)
+        );
+    }
+
+    @Test
+    void noTax() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 17777, 0, 0.09)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(1481),
+                Collections.singletonList(0),
+                Collections.singletonList(133),
+                Collections.singletonList(1481)
+        );
+    }
+
+    @Test
+    void secondTierTax() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 35000, 0, 0.09)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(2917),
+                Collections.singletonList(266),
+                Collections.singletonList(263),
+                Collections.singletonList(2651)
+        );
+    }
+
+    @Test
+    void thirdTierTax() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 60050, 0, 0.09)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(5004),
+                Collections.singletonList(922),
+                Collections.singletonList(450),
+                Collections.singletonList(4082)
+        );
+    }
+
+
+    @Test
+    void fourthTierTax() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 120000, 0, 0.09)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(10000),
+                Collections.singletonList(2669),
+                Collections.singletonList(900),
+                Collections.singletonList(7331)
+        );
+    }
+
+    @Test
+    void fifthTierTax() {
+        var employeeService = new EmployeeService();
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 200000, 0, 0.09)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(16667),
+                Collections.singletonList(5269),
+                Collections.singletonList(1500),
+                Collections.singletonList(11398)
+        );
+    }
+
+    // -----------------------   Helpers   -----------------------------
     void testPaySlips(List<Payslip> payslips,
                       List<Employee> employees,
                       List<String> expectedFromDates,
@@ -29,28 +178,4 @@ class EmployeeServiceTest {
             assertEquals(paySlip.getNetIncome(), expectedNetIncomes.get(i));
         }
     }
-
-    @Test
-    void baseTest() {
-        var employeeService = new EmployeeService();
-        var employees = Collections.singletonList(
-                new Employee("David", "Tan", 0, 0, 0)
-        );
-        var payslips = employeeService.generatePayslip(employees);
-        testPaySlips(payslips,
-                employees,
-                Collections.singletonList("01 January"),
-                Collections.singletonList("31 January"),
-                Collections.singletonList(0),
-                Collections.singletonList(0),
-                Collections.singletonList(0),
-                Collections.singletonList(0)
-        );
-    }
-
-    // PayPeriod tests
-//    void january();
-//    void september();
-//    void thisFeburary();
-
 }
