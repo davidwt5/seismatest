afa7db724805cfee40fb8fcf45a2704e279cce45
davidwt5 <davidwtan75@gmail.com>
2022-05-22 14:33:36 +1000

    added testing support for multiple employees

--- src/test/java/com/davidwtan/seismatest/services/EmployeeServiceTest.java
+++ src/test/java/com/davidwtan/seismatest/services/EmployeeServiceTest.java
@@ -1,30 +1,51 @@
 package com.davidwtan.seismatest.services;
 
 import com.davidwtan.seismatest.models.Employee;
+import com.davidwtan.seismatest.models.Payslip;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeServiceTest {
+    void testPaySlips(List<Payslip> payslips,
+                      List<Employee> employees,
+                      List<String> expectedFromDates,
+                      List<String> expectedToDates,
+                      List<Integer> expectedGrossIncomes,
+                      List<Integer> expectedIncomeTaxes,
+                      List<Integer> expectedSuperannuations,
+                      List<Integer> expectedNetIncomes) {
+        for(int i=0; i<payslips.size(); i++) {
+            var paySlip = payslips.get(i);
+            assertEquals(paySlip.getEmployee(), employees.get(i));
+            assertEquals(paySlip.getFromDate(), expectedFromDates.get(i));
+            assertEquals(paySlip.getToDate(), expectedToDates.get(i));
+            assertEquals(paySlip.getGrossIncome(), expectedGrossIncomes.get(i));
+            assertEquals(paySlip.getIncomeTax(), expectedIncomeTaxes.get(i));
+            assertEquals(paySlip.getSuperannuation(), expectedSuperannuations.get(i));
+            assertEquals(paySlip.getNetIncome(), expectedNetIncomes.get(i));
+        }
+    }
+
     @Test
     void baseTest() {
         var employeeService = new EmployeeService();
-        var employees = new ArrayList<Employee>();
-        employees.add(new Employee("David", "Tan", 0, 0, 0));
-
-        var paySlips = employeeService.generatePayslip(employees);
-        for(int i=0; i<paySlips.size(); i++) {
-            var paySlip = paySlips.get(i);
-            assertEquals(paySlip.getEmployee(), employees.get(i));
-            assertEquals(paySlip.getFromDate(), "01 January");
-            assertEquals(paySlip.getToDate(), "31 January");
-            assertEquals(paySlip.getGrossIncome(), 0);
-            assertEquals(paySlip.getIncomeTax(), 0);
-            assertEquals(paySlip.getSuperannuation(), 0);
-            assertEquals(paySlip.getNetIncome(), 0);
-        }
+        var employees = Collections.singletonList(
+                new Employee("David", "Tan", 0, 0, 0)
+        );
+        var payslips = employeeService.generatePayslip(employees);
+        testPaySlips(payslips,
+                employees,
+                Collections.singletonList("01 January"),
+                Collections.singletonList("31 January"),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0),
+                Collections.singletonList(0)
+        );
     }
 
     // PayPeriod tests
