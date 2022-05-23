04bb018e58e34704f8b4dc6af554ad382610e0f9
davidwt5 <davidwtan75@gmail.com>
2022-05-23 13:50:07 +1000

    fixed warning

--- src/main/java/com/davidwtan/seismatest/controllers/EmployeeController.java
+++ src/main/java/com/davidwtan/seismatest/controllers/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @Autowired
     public EmployeeController(EmployeeService employeeService) {
