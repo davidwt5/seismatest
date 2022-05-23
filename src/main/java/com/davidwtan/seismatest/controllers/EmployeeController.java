a7f485977c0792f4e32f3b26beffad419b5b1545
davidwt5 <davidwtan75@gmail.com>
2022-05-23 13:49:08 +1000

    added dummy route to test redeployment

--- src/main/java/com/davidwtan/seismatest/controllers/EmployeeController.java
+++ src/main/java/com/davidwtan/seismatest/controllers/EmployeeController.java
@@ -4,10 +4,7 @@ import com.davidwtan.seismatest.models.Employee;
 import com.davidwtan.seismatest.models.Payslip;
 import com.davidwtan.seismatest.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class EmployeeController {
     @PostMapping("/payslip")
     public List<Payslip> payslip(@RequestBody List<Employee> employees) {
         return employeeService.generatePayslip(employees);
+    }
+
+    @GetMapping("/payslip")
+    public String payslip() {
+        return "payslip :)";
     }
 }
